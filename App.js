07eed255f12b32e822bb776bddcc32496eaf63cf
Christian <caay00000@gmail.com>
2023-12-07 17:29:27 -0600

    Segundo commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Primer cambio</Text>
+      <Text>Segundo cambio</Text>
       <StatusBar style="auto" />
     </View>
   );
