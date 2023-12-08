7170d8e27dbe56cb9c3cd6ff25d9607c0dbcca0f
Christian <caay00000@gmail.com>
2023-12-07 18:10:41 -0600

    Tercer cambio

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Segundo cambio</Text>
+      <Text>Segundo cambio.</Text>
       <StatusBar style="auto" />
     </View>
   );
